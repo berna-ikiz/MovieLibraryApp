c4aa72bbf389c70b77891b3910075ce528b0bb66
Berna İkiz <34381430+berna-ikiz@users.noreply.github.com>
2025-05-18 01:53:07 +0300

    Flatlist has been added to show searched results on the search screen

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,9 +1,17 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import Colors from "../theme/colors";
 import { TextInput } from "react-native-gesture-handler";
 import { searchMovies } from "../services/movieApi";
 import Loading from "../components/Loading";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 const SearchScreen = () => {
   const [activeTab, setActiveTab] = useState("Search");
@@ -16,6 +24,7 @@ const SearchScreen = () => {
         setIsLoading(true);
         try {
           const results = await searchMovies(searchText);
+          setResults(results);
           console.log(results);
         } catch (error) {
           //TODO toast message
@@ -72,12 +81,43 @@ const SearchScreen = () => {
               onChangeText={setSearchText}
             />
             {isLoading && <Loading title={""} />}
+            <FlatList
+              data={results}
+              keyExtractor={(item) => item.id.toString()}
+              renderItem={({ item }) => renderItem({ item })}
+              numColumns={2}
+              columnWrapperStyle={styles.columnWrapper}
+            />
           </>
         )}
         {activeTab === "filter" && (
           <Text style={styles.tabText}>Filter UI here</Text>
         )}
       </View>
+    </View>
+  );
+};
+
+const renderItem = ({ item }: any) => {
+  console.log(item);
+  return (
+    <View style={styles.searchCard}>
+      {item.poster_path ? (
+        <Image
+          source={{
+            uri: `https://image.tmdb.org/t/p/w500${item.poster_path}`,
+          }}
+          style={styles.poster}
+          resizeMode="cover"
+        />
+      ) : (
+        <View style={[styles.poster, styles.noImageIconContainer]}>
+          <Icon name="movie-roll" size={40} color={Colors.gray800} />
+        </View>
+      )}
+      <Text style={styles.movieTitle} numberOfLines={2}>
+        {item.title}
+      </Text>
     </View>
   );
 };
@@ -123,5 +163,36 @@ const styles = StyleSheet.create({
     borderRadius: 6,
     color: Colors.gray400,
     fontSize: 20,
+  },
+  searchCard: {
+    flex: 1,
+    margin: 8,
+    borderRadius: 8,
+    backgroundColor: Colors.black,
+    overflow: "hidden",
+    alignItems: "center",
+  },
+  poster: {
+    width: "100%",
+    height: 180,
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+  },
+  movieTitle: {
+    color: Colors.gray300,
+    fontSize: 20,
+    padding: 8,
+    textAlign: "center",
+  },
+  noImageIconContainer: {
+    borderWidth: 1,
+    borderColor: Colors.gray700,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  columnWrapper: {
+    justifyContent: "space-between",
+    marginBottom: 16,
+    paddingHorizontal: 10,
   },
 });
